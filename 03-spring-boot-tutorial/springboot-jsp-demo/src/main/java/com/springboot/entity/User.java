9adc58a16df4d556cef364ca94b1093695531fbc
Bui Hoa <hoabrum@gmail.com>
2026-01-27 08:07:15 +0700

    Create User entity

---
+++ 03-spring-boot-tutorial/springboot-jsp-demo/src/main/java/com/springboot/entity/User.java
@@ -0,0 +1,63 @@
+package com.springboot.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users", schema = "dbo")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "address")
+    private String address;
+
+    public User() {
+    }
+
+    public User(String email, String password, String address) {
+        this.email = email;
+        this.password = password;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
